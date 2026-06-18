0416822a88aafc30696ec6afb1d7f37eded53923
Abrahão Lopes <117474324+abrahao2k@users.noreply.github.com>
2026-06-18 15:05:13 -0300

    Delete desvio4.py

--- desvio4.py
+++
@@ -1,9 +0,0 @@
-escola = input("Qual é a melhor escola de Mossoró? ")
-
-if escola.lower() == "ifrn": # lower - transforma em
-    print("Acertou!!!!!!")   # letras minúsculas
-
-else:
-    print("Errooooouuuu!!!".upper() )
-    
-# .upper() - transforma em letras maiúsculas
